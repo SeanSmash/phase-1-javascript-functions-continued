3665478dfcd5f9b255122206e88f11e800fa23ff
Sean Sarokas <sps4462@gmail.com>
2022-04-08 10:07:00 -0400

    commit tree 12a7df214a2675cb8f9f41da951e6ec100f5d36b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(someActivity){
+    return (Boolean(someActivity)) ? (`This Saturday, I want to ${someActivity}!`) : 
+    (`This Saturday, I want to roller-skate!`);
+}
+
+function mondayWork(workActivity){
+    return (Boolean(workActivity)) ? (`This Monday, I will ${workActivity}.`) : 
+    (`This Monday, I will go to the office.`);
+}
+
+function wrapAdjective(highlight){
+    return function(msg = "special"){
+        return `You are ${highlight}${msg}${highlight}!`;
+    }
+}
